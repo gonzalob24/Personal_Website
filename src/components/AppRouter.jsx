f7c25b3ca4b52a64bc10020bff34c82ab9b39abc
Gonzalo <gonzalobetancourt@me.com>
2024-10-01 13:14:13 -0500

    update route for github pages

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -7,7 +7,7 @@ function AppRouter() {
 		<BrowserRouter>
 			<NavBar />
 			<Routes>
-				<Route path="/Portfolio_Website" exact element={<App />} />
+				<Route path="/Personal_Website" exact element={<App />} />
 			</Routes>
 		</BrowserRouter>
 	);
