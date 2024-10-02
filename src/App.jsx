06cb494a8b31dc09cc651b86cf41e3b36bc2dae3
Gonzalo <gonzalobetancourt@me.com>
2024-10-01 23:09:55 -0500

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Portfolio from './components/Portfolio';
 import { ThemeProvider } from '@emotion/react';
 import theme from './styles/theme';
 import ContactMe from './components/ContactMe';
+import Footer from './components/Footer';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<AboutMe />
 			<Portfolio />
 			<ContactMe />
+			<Footer />
 		</ThemeProvider>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import { Box, BottomNavigation, Typography, Link, Button } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { FaArrowCircleUp } from 'react-icons/fa';
+import CustomLink from './CustomLink';
+
+function Footer() {
+	return (
+		<Box display={'flex'} justifyContent="space-between" px="10px" component="footer" sx={{ py: 3, bgcolor: (theme) => theme.palette.grey[900] }}>
+			<Typography alignSelf="center" component="span" variant="inherit" textTransform={'uppercase'}>
+				Gonzalo Betancourt ©{new Date().getFullYear()}
+			</Typography>
+			<Grid>
+				<Button padding={2} href="#landing">
+					<FaArrowCircleUp size={30} color={'#d500f9'} />
+				</Button>
+			</Grid>
+		</Box>
+	);
+}
+
+export default Footer;
